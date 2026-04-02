9101dab5789e8cac9bdcc68029260fe9ef715e37
jwhtmx <wchia3@illinois.edu>
2026-04-01 19:49:30 -0500

    Reapply "Update GameManager.cs"
    This reverts commit 75de16187ddb00830d28bcebe236a157a601250c.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -455,6 +455,26 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void DespawnAllOompaLoompas()
+    {
+        OompaLoompa[] oompas = FindObjectsOfType<OompaLoompa>();
+
+        for (int i = 0; i < oompas.Length; i++)
+        {
+            Destroy(oompas[i].gameObject);
+        }
+    }
+
+    private void SpawnCurrentOompaLoompas()
+    {
+        if (oompaLoompaCount <= 0) return;
+
+        for (int i = 0; i < oompaLoompaCount; i++)
+        {
+            SpawnOompaLoompa();
+        }
+    }
+
     private void ApplyCoffeePanelStateInstant()
     {
         if (coffeePanel == null) return;
@@ -602,6 +622,8 @@ public class GameManager : MonoBehaviour
     {
         string savedPrestigeStars = preservePrestigeStars ? prestigeStars : "";
 
+        DespawnAllOompaLoompas();
+
         donutCount = 0f;
         coffeeCount = 0f;
         oompaLoompaCount = 1;
@@ -619,6 +641,7 @@ public class GameManager : MonoBehaviour
         PlayerPrefs.DeleteAll();
         SaveGame();
 
+        SpawnCurrentOompaLoompas();
         ApplyCoffeePanelStateInstant();
     }
 }
